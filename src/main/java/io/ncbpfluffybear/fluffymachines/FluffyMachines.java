787ba2ce2ab6fb1340f83555a35fa9adce95faea
ybw0014 <i@guizhanss.com>
2022-08-30 14:53:38 -0400

    更新前置

--- src/main/java/io/ncbpfluffybear/fluffymachines/FluffyMachines.java
+++ src/main/java/io/ncbpfluffybear/fluffymachines/FluffyMachines.java
@@ -4,7 +4,6 @@ import io.github.thebusybiscuit.slimefun4.api.SlimefunAddon;
 import io.github.thebusybiscuit.slimefun4.api.player.PlayerProfile;
 import io.github.thebusybiscuit.slimefun4.libraries.dough.collections.Pair;
 import io.github.thebusybiscuit.slimefun4.libraries.dough.config.Config;
-import io.github.thebusybiscuit.slimefun4.libraries.dough.updater.GitHubBuildsUpdater;
 import io.ncbpfluffybear.fluffymachines.listeners.KeyedCrafterListener;
 import io.ncbpfluffybear.fluffymachines.utils.Constants;
 import io.ncbpfluffybear.fluffymachines.utils.Events;
@@ -24,7 +23,7 @@ import javax.annotation.Nonnull;
 
 import lombok.SneakyThrows;
 import me.mrCookieSlime.Slimefun.api.BlockStorage;
-import net.guizhanss.guizhanlib.updater.GuizhanBuildsUpdater;
+import net.guizhanss.guizhanlibplugin.updater.GuizhanBuildsUpdaterWrapper;
 import org.bstats.bukkit.Metrics;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -55,7 +54,7 @@ public class FluffyMachines extends JavaPlugin implements SlimefunAddon {
         Config cfg = new Config(this);
 
         if (cfg.getBoolean("options.auto-update") && getDescription().getVersion().startsWith("Build ")) {
-            new GuizhanBuildsUpdater(this, getFile(), "baoad", "FluffyMachines", "master", false, "zh-CN").start();
+            GuizhanBuildsUpdaterWrapper.start(this, getFile(), "baoad", "FluffyMachines", "master", false);
         }
 
         // Register Glow
